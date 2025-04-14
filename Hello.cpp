1e6ec5b5f82c423b7848f41e89e02dc0a6d7dff0
Houssem Eddine LASSOUED <houssemeddine.lassoued@gmail.com>
2025-04-14 13:53:33 -0400

    ajout du fichier Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Nombres pairs de 1 à 20 :" << std::endl;
+
+    for (int i = 1; i <= 20; ++i) {
+        if (i % 2 == 0) {
+            std::cout << i << " ";
+        }
+    }
+
+    std::cout << std::endl;
+    return 0;
+}

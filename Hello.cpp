c7e2f9da64748d01edaa266bbf530f855f7d7455
Houssem Eddine LASSOUED <houssemeddine.lassoued@gmail.com>
2025-04-14 14:07:46 -0400

    modification du message

--- Hello.cpp
+++ Hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Nombres pairs de 1 à 20 :" << std::endl;
+    std::cout << "Nombres pairs de 1 à 30 :" << std::endl;
 
     for (int i = 1; i <= 30; ++i) {
         if (i % 2 == 0) {
